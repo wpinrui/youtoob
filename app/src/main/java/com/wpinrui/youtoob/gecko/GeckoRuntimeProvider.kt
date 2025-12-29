ec46b56b2077c4127001cb7ede24d7106ed58990
Ivan <77185900+wpinrui@users.noreply.github.com>
2025-12-30 01:28:48 +0800

    Add autoplay settings to GeckoRuntimeSettings

--- app/src/main/java/com/wpinrui/youtoob/gecko/GeckoRuntimeProvider.kt
+++ app/src/main/java/com/wpinrui/youtoob/gecko/GeckoRuntimeProvider.kt
@@ -2,6 +2,7 @@ package com.wpinrui.youtoob.gecko
 
 import android.content.Context
 import org.mozilla.geckoview.GeckoRuntime
+import org.mozilla.geckoview.GeckoRuntimeSettings
 
 object GeckoRuntimeProvider {
     @Volatile
@@ -9,10 +10,17 @@ object GeckoRuntimeProvider {
 
     fun getRuntime(context: Context): GeckoRuntime {
         return runtime ?: synchronized(this) {
-            runtime ?: GeckoRuntime.create(context.applicationContext).also {
+            runtime ?: createRuntime(context).also {
                 runtime = it
             }
         }
+    }
+
+    private fun createRuntime(context: Context): GeckoRuntime {
+        val settings = GeckoRuntimeSettings.Builder()
+            .autoplayDefault(GeckoRuntimeSettings.AUTOPLAY_DEFAULT_ALLOWED)
+            .build()
+        return GeckoRuntime.create(context.applicationContext, settings)
     }
 
     fun shutdown() {
